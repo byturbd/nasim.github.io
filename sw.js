800774b0bf6c2327742c65f49dd628e1f48e239b
byturbd <abzabbar.official@gmail.com>
2026-03-06 11:19:50 +0600

    Nasim

--- sw.js
+++ sw.js
@@ -1,38 +1,20 @@
-const CACHE_NAME = 'nasim-store-v2';
-const ASSETS_TO_CACHE = [
-  'index.html',
-  'manifest.json',
+const CACHE_NAME = 'nasim-store-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
   'https://cdn.tailwindcss.com',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
 ];
 
-// ইনস্টল ইভেন্ট - ফাইলগুলো ক্যাশ করা
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Caching assets...');
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
-  );
-  self.skipWaiting();
-});
-
-// অ্যাক্টিভেট ইভেন্ট - পুরাতন ক্যাশ মুছে ফেলা
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// ফেচ ইভেন্ট - অফলাইনে অ্যাপ চালানো
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request);
-    })
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
